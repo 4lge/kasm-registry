f461deaba60b7b9f5a596c42e505b47ab9262a27
Albrecht Gebhardt <albrecht.gebhardt@aau.at>
2025-09-30 19:39:09 +0200

    fix url

--- site/next.config.js
+++ site/next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: 'https://4lge.algepop.net/kasm-registry/1.0/ubuntu.png',
-    listUrl: 'https://4lge.algepop.net/kasm-registry/1.0/',
-    contactUrl: 'https://4lge.algepop.net/ksam-registry/support',
+    icon: 'https://4lge.github.io/kasm-registry/1.1/ubuntu.png',
+    listUrl: 'https://4lge.github.io/kasm-registry/1.1/',
+    contactUrl: 'https://4lge.github.io/ksam-registry/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
