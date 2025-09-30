bab4703bad5db13e3518ed6909c9d2157209df43
Albrecht Gebhardt <albrecht.gebhardt@aau.at>
2025-09-30 16:54:34 +0200

    use .hasOwnProperty()

--- processing/processjson.js
+++ processing/processjson.js
@@ -44,12 +44,12 @@ glob("**/workspace.json", async function (err, files) {
 		parsed.sha = hash.hash;
 		console.log(parsed.friendly_name + ' added')
 		parsed.compatibility.forEach((element, index) => {
-			if (element.contains('available_tags')) {
+			if (element..hasOwnProperty('available_tags')) {
 				element.available_tags.forEach((el) => {
 					channels.add(el)
 				})
 			}
-			if (element.contains('version')) {
+			if (element.hasOwnProperty('version')) {
 				versions.add(element.version)
 			}
 		})
